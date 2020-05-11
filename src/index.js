183c03d30b122000986be1947346a294ac7f0b3c
carl-larson <clarson08@gmail.com>
2020-05-11 18:41:29 -0500

    put in the index code for routers, probably needs work

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const express = require ('express')
+const playersRouter = require('./routers/players');
+const authRouter = require('./routers/auth');
+const bodyParser = require('body-parser');
+require ('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.use('/players', playersRouter);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my Capstone server!')
+    })
+
+    app.listen(port, () => {
+    console.log(`Web server is listening on port ${port}!`);
+    });
+
 
 ReactDOM.render(
   <React.StrictMode>
