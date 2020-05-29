ab4a293028aa88c6a2d28850131941e76a9c5669
carl-larson <clarson08@gmail.com>
2020-05-28 19:55:17 -0500

    making the game visible with react

--- src/Row.js
+++ src/Row.js
@@ -7,10 +7,34 @@ class Row extends React.Component {
             die1: 1
         }
     }
+
     render() {
+        // let thisDie = document.getElementById(name).firstChild;
+        if (value == 1) {
+            thisDie.style.backgroundPosition = '0px 0px';
+        }
+        if (value == 2) {
+            thisDie.style.backgroundPosition = '-64px 0px';
+        }
+        if (value == 3) {
+            thisDie.style.backgroundPosition = '0px -64px';
+        }
+        if (value == 4) {
+            thisDie.style.backgroundPosition = '-64px -64px';
+        }
+        if (value == 5) {
+            thisDie.style.backgroundPosition = '0px -128px';
+        }
+        if (value == 6) {
+            thisDie.style.backgroundPosition = '-64px -128px';
+        }
+    
+        }
         return (
             <div className='row'>
-                <div className={`rolled${this.props.row}`}></div>
+                <div className={`rolled${this.props.row}`}>
+                    <img src='./farkle/dicePic.png'></img>
+                </div>
                 <div className={`kept${this.props.row}`}></div>
             </div>
         )
