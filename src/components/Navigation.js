2dedd33c960f3b9189e9c2a197390adb00c5114b
carl-larson <clarson08@gmail.com>
2020-07-15 21:42:33 -0500

    Revert "updated the login page to work more like the one I built in capstone backend"
    This reverts commit aab4f3862e0aca3f5e33b572f8e19a412e86d6ba.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,18 +7,10 @@ const Navigation = () => {
     return (
         <div>
             <h1>Farkle!</h1>
-            <div className='navBar'>
-                <ul className='navButtonList'>
-                    <li>
-                        <a className='navButton active' href="/">Home</a>
-                    </li>
-                    <li>
-                        <a className='navButton active' href="/login">Login</a>
-                    </li>
-                    <li>
-                        <a className='navButton active' href="/farkle">Play</a>
-                    </li>
-                </ul>
+            <div className='buttons'>
+                <a activeClassName="active" href="/">Home</a>
+                <a activeClassName="active" href="/login">Login</a>
+                <a activeClassName="active" href="/farkle">Play</a>
             </div>
         </div>
     )
