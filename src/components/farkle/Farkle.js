9869629eb115856b3d9c7a2d237e78791a0e53c0
carl-larson <clarson08@gmail.com>
2020-07-18 20:28:58 -0500

    got the basic dice layout and background url working.

--- src/components/farkle/Farkle.js
+++ src/components/farkle/Farkle.js
@@ -1,24 +1,72 @@
 import React from 'react';
-import Row from './Row'
+import Row from './Row';
 
-import 'farkleStyle.css'
+import './farkleStyle.css';
 
 class Farkle extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            die1: 1
+            dice: [
+                {
+                    value: 1,
+                    selected: false,
+                    kept: false
+                },
+                {
+                    value: 2,
+                    selected: false,
+                    kept: false
+                },
+                {
+                    value: 3,
+                    selected: false,
+                    kept: false
+                },
+                {
+                    value: 4,
+                    selected: true,
+                    kept: false
+                },
+                {
+                    value: 5,
+                    selected: false,
+                    kept: false
+                },
+                {
+                    value: 6,
+                    selected: false,
+                    kept: false
+                }
+            ]
+            
         }
     }
+    rollDice = () => {
+        let newRoll = 0;
+        this.state.dice.map((die, ind) => {
+            if(die.selected === false) {
+                newRoll = Math.ceil(Math.random()*6);
+                console.log(newRoll);
+            } else {
+                newRoll = die.value;
+                die.kept = true;
+            }
+            console.log('die value', die.value)
+            return die.value = newRoll;
+        })
+    }
     render() {
+        let die = this.state.dice;
         return (
             <div className='board'>
-                <Row row={1}></Row>
-                <Row className='row2' row={2}></Row>
-                <Row className='row3' row={3}></Row>
-                <Row className='row4' row={4}></Row>
-                <Row className='row5' row={5}></Row>
-                <Row className='row6' row={6}></Row>
+                <Row className='row1' value={die[0].value} selected={die[0].selected} kept={die[0].kept}></Row>
+                <Row className='row2' value={die[1].value} selected={die[1].selected} kept={die[1].kept}></Row>
+                <Row className='row3' value={die[2].value} selected={die[2].selected} kept={die[2].kept}></Row>
+                <Row className='row4' value={die[3].value} selected={die[3].selected} kept={die[3].kept}></Row>
+                <Row className='row5' value={die[4].value} selected={die[4].selected} kept={die[4].kept}></Row>
+                <Row className='row6' value={die[5].value} selected={die[5].selected} kept={die[5].kept}></Row>
+                <button onClick={this.rollDice}>Roll!</button>
             </div>
 
         )
--- src/components/farkle/Row.js
+++ src/components/farkle/Row.js
@@ -4,38 +4,53 @@ class Row extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
-            value: 1
+            value: Number(props.value),
+            selected: props.selected,
+            kept: props.kept
         }
     }
 
     render() {
-        let value = this.state.value;
-        // let thisDie = document.getElementById(name).firstChild;
-        if (value == 1) {
-            // thisDie.style.backgroundPosition = '0px 0px';
-        }
-        if (value == 2) {
-            // thisDie.style.backgroundPosition = '-64px 0px';
-        }
-        if (value == 3) {
-            // thisDie.style.backgroundPosition = '0px -64px';
-        }
-        if (value == 4) {
-            // thisDie.style.backgroundPosition = '-64px -64px';
-        }
-        if (value == 5) {
-            // thisDie.style.backgroundPosition = '0px -128px';
-        }
-        if (value == 6) {
-            // thisDie.style.backgroundPosition = '-64px -128px';
-        }
+        // let value = this.props.value;
+        // let selected = this.props.selected;
+        // let kept = this.props.kept;
+        console.log(this.state.value, this.state.selected, this.state.kept)
+        let xRolledPosition = 0;
+        let xKeptPosition = -64;
+        let yPosition = -((this.state.value - 1) * 64);
+        let rolledPosition = `${xRolledPosition}px ${yPosition}px`;
+        let keptPosition = `${xKeptPosition}px ${yPosition}px`;
+        let bordercolor = 'gray';
+        this.state.selected ? bordercolor = 'red' : bordercolor = 'gray';
+        // if (value === 1) {
+        //     diePosition = '0px 0px';
+        // }
+        // if (value === 2) {
+        //     diePosition = '-64px 0px';
+        // }
+        // if (value === 3) {
+        //     diePosition = '0px -64px';
+        // }
+        // if (value === 4) {
+        //     diePosition = '-64px -64px';
+        // }
+        // if (value === 5) {
+        //     diePosition = '0px -128px';
+        // }
+        // if (value === 6) {
+        //     diePosition = '-64px -128px';
+        // }
+        // if (kept === false) {
+        //     diePosition = '0px -192px';
+        // }
     
         return (
             <div className='row'>
-                <div className={`rolled${this.props.row}`}>
-                    <img src='./farkle/dicePic.png'></img>
+                <div className='rolled dice'
+                    style={{backgroundPosition: rolledPosition, borderColor: bordercolor}}>
                 </div>
-                <div className={`kept${this.props.row}`}></div>
+                <div className='kept dice' style={{backgroundPosition: keptPosition}}>
+                </div>
             </div>
         )
     }
