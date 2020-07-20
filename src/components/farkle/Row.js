dedd1e8f15ce68929d640c07dd3402c80fe22b39
carl-larson <clarson08@gmail.com>
2020-07-20 12:33:07 -0500

    Made a bunch of changes to Rowjs to move things into state and componentdidupdate but it no longer works and i'm not sure it was worthwhile.

--- src/components/farkle/Row.js
+++ src/components/farkle/Row.js
@@ -1,49 +1,81 @@
 import React from 'react';
 
 class Row extends React.Component {
-    // constructor (props) {
-    //     super(props);
-    //     this.state = {
-    //         value: Number(props.value),
-    //         selected: props.selected,
-    //         kept: props.kept
-    //     }
-    // }
+    constructor (props) {
+        super(props);
+        this.state = {
+            // value: Number(props.value),
+            // selected: props.selected,
+            // kept: props.kept
+        
+            xRolledPosition: 0,
+            xKeptPosition: -64,
+            yPosition: -((this.props.value - 1) * 64),
+            selectedcolor: 'gray',
+            keptcolor: 'gray',
+            rolledPosition: `${this.xRolledPosition}px ${this.yPosition}px`,
+            keptPosition: `${this.xKeptPosition}px ${this.yPosition}px`
+        };
+    }
+    
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props !== prevProps)
+        {console.log(this.props.value, this.props.selected, this.props.kept)
+        if (this.props.selected && !this.props.kept) {
+            this.setState({selectedcolor: 'red'});
+        } else if (this.props.selected && this.props.kept) {
+            this.setState({selectedcolor: 'gray',
+                keptcolor: 'red', 
+                xRolledPosition: -64,
+                xKeptPosition: 0});
+        } else {
+            this.setState({selectedcolor: 'gray',
+            keptcolor: 'gray',
+            xRolledPosition: 0,
+            xKeptPosition: -64});
+        }}
+
+
+    }
 
     render() {
-        let value = this.props.value;
-        let selected = this.props.selected;
-        let kept = this.props.kept;
-        console.log(value, selected, kept)
-        let xRolledPosition = 0;
-        let xKeptPosition = -64;
-        let yPosition = -((value - 1) * 64);
+        // let value = this.props.value;
+        // let selected = this.props.selected;
+        // let kept = this.props.kept;
+        // console.log(value, selected, kept)
+        // let xRolledPosition = 0;
+        // let xKeptPosition = -64;
+        // let yPosition = -((value - 1) * 64);
         
-        let selectedcolor = 'gray';
-        let keptcolor = 'gray';
+        // let selectedcolor = 'gray';
+        // let keptcolor = 'gray';
         // selected ? bordercolor = 'red' : bordercolor = 'gray';
-        if (selected && !kept) {
-            selectedcolor = 'red';
-        } else if (selected && kept) {
-            selectedcolor = 'gray';
-            keptcolor = 'red';
-            xRolledPosition = -64;
-            xKeptPosition = 0;
-        } else {
-            selectedcolor = 'gray';
-            keptcolor = 'gray';
-            xRolledPosition = 0;
-            xKeptPosition = -64;
-        }
-        let rolledPosition = `${xRolledPosition}px ${yPosition}px`;
-        let keptPosition = `${xKeptPosition}px ${yPosition}px`;
+        // if (this.props.selected && !this.props.kept) {
+        //     this.selectedcolor = 'red';
+        // } else if (this.props.selected && this.props.kept) {
+        //     this.selectedcolor = 'gray';
+        //     this.keptcolor = 'red';
+        //     this.xRolledPosition = -64;
+        //     this.xKeptPosition = 0;
+        // } else {
+        //     this.selectedcolor = 'gray';
+        //     this.keptcolor = 'gray';
+        //     this.xRolledPosition = 0;
+        //     this.xKeptPosition = -64;
+        // }
+        // let rolledPosition = `${this.xRolledPosition}px ${this.yPosition}px`;
+        // let keptPosition = `${this.xKeptPosition}px ${this.yPosition}px`;
 
         return (
             <div className='row'>
                 <div className='rolled dice'
-                    style={{backgroundPosition: rolledPosition, borderColor: selectedcolor}}>
+                    style={{backgroundPosition: this.rolledPosition, borderColor: this.selectedcolor}}>
                 </div>
-                <div className='kept dice' style={{backgroundPosition: keptPosition, borderColor: keptcolor}}>
+                <div className='kept dice' style={{backgroundPosition: this.keptPosition, borderColor: this.keptcolor}}>
                 </div>
             </div>
         )
